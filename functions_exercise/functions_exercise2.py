2a66e84b621fb262b05896c859269c525169c669
kibezjhb025 <kibezjhb025@student.wethinkcode.co.za>
2025-11-02 21:48:02 +0200

    passed all test but 1. NB - to discuss

--- functions_exercise/functions_exercise2.py
+++ functions_exercise/functions_exercise2.py
@@ -1,7 +1,7 @@
 # Exercise 2: Functions with and without Parameters
 # We will also put into practice what you did or learned on control structures
 # Please mind your arguments!!!!
-
+import sys
 
 # 1. Write a function without a parameter called 'get_user_input'
     # This function must take user inputs 'Username, age and email'
@@ -10,6 +10,25 @@
         # If the AGE is less than 17 print "You are young"
         # If the Age is more than 65 print "You are too old"  
     # Return all the inputs
+def get_user_input(username = None, age= None, email =None ):
+    if username is None:
+        username = input("Enter your username: ")
+    
+    while True:
+        age_str = input("Enter your age: ")
+        if not str(age_str).isdigit():
+            print("Not an Integer, Enter a valid age!")
+            continue
+    
+        age = int(age_str)
+        if age < 17:
+            print("You are young")
+        elif age > 65:
+            print("You are too old")
+        break
+
+    email = input("Enter your email: ")
+    return username, age, email
 
 
 
@@ -21,9 +40,14 @@
     # If the username is an empty string return False
     # Return the boolean
 
-def validate_Username():
-   pass
-
+def validate_username(username):
+    
+    if username == "":
+        return False
+    for char in username:
+        if not char.isalpha():
+            return False
+    return True
 
 # 3. Pass the parameters to the function below 'display_user_info'
     # Underneath the User Information print the following:
@@ -32,8 +56,11 @@ def validate_Username():
         # 'Email    : <email>
     # return 'Thanks!, Details captured.'
 
-def display_user_info():
-    pass
+def display_user_info(username, age, email, output=sys.stdout):
+   
+    print(f"\nUser Information:\nUsername : {username}\nAge      : {age}\nEmail    : {email}", file=output)
+    return "Thanks!, Details captured."
+
    
 
 # 4. Call all the functions
@@ -41,9 +68,14 @@ def display_user_info():
         # write a loop to and take new input for the username and take it to be evaluated 'validate_username'
     # if 'validate_username' returns True then you can display the user Info   
 def main():
-    pass
+    username, age, email = get_user_input()
 
+    while not validate_username(username):
+        print("Invalid username. Only letters allowed and cannot be empty.")
+        username = input("Enter a valid username: ")
+
+    message = display_user_info(username, age, email)
+    print(message)
 
 if __name__ == "__main__":
-   #call the main function
-    pass
+    main()
