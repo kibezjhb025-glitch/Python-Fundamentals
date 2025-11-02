fbc1f79ca330bee2e1b36c039b9ade75824e00d9
kibezjhb025 <kibezjhb025@student.wethinkcode.co.za>
2025-11-02 21:48:33 +0200

    completed functions

--- functions_exercise/functions_exercise.py
+++ functions_exercise/functions_exercise.py
@@ -8,7 +8,7 @@
 
 def add_numbers(a, b):
     # Your code here
-    pass
+    print("Sum:", a + b)
 
 # Exercise 2: Return Values
 # Instructions:
@@ -19,8 +19,8 @@ def add_numbers(a, b):
 # as a parameter and returns the sum of those numbers.
 
 def calculate_sum(numbers):
-    # Your code here
-    pass
+    
+    return sum(numbers)
 
 # Exercise 3: Function Scoping
 # Instructions:
@@ -34,7 +34,10 @@ global_variable = "I am a global variable"
 def demonstrate_scoping():
     # Define a local variable here
     # Print both the global and local variables
-    pass
+    local_variable = "I am a local variable"
+    print(f"Global variable: {global_variable}")
+    print(f"Local variable: {local_variable}")
+
 
 # Exercise 4: Function Libraries
 # Instructions:
@@ -47,15 +50,15 @@ def demonstrate_scoping():
     # and another that calculates the area of a circle.
 
 # Import your custom function library
-
+from math_models import rectangle_area, circle_area
 
 def calculate_area(length, width, radius):
     # Example 1: 
-    area_rectangle = 1 # Use the rectangle_area function from the Python module you created 
+    area_rectangle = rectangle_area(length, width) # Use the rectangle_area function from the Python module you created 
     print("Area of the rectangle:", area_rectangle)
 
     # Example 2: 
-    area_circle = 2 # Use the circle_area function from the Python module you created
+    area_circle = circle_area(radius) # Use the circle_area function from the Python module you created
     print("Area of the circle:", area_circle)
 
 if __name__ == "__main__":
